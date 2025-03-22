c477654713c617df62e04a0756c9062564778745
homile <ssanho2@naver.com>
2025-03-23 02:40:53 +0900

    mod: kakao_sdk 버전 변경, env 설정 추가, android build 이슈 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk_template.dart';
 import 'package:provider/provider.dart';
 import 'package:groomingo/routes/routes.dart';
 
 import 'package:groomingo/viewmodels/signin_viewmodel.dart';
 import 'package:groomingo/styles/colors.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Hide status bar
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
       overlays: [SystemUiOverlay.bottom]);
+  await dotenv.load(fileName: ".env");
+
+  KakaoSdk.init(nativeAppKey: dotenv.get("KAKAO_NATIVE_APP_KEY"));
+
   runApp(
     MultiProvider(
       providers: [
